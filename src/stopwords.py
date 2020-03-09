66b18e25d07f2ff02e8c3c6827c7ca71b68d6e90
Gleb Fedorov <glebfedorov888@gmail.com>
2020-03-09 15:46:22 +0100

    Added list of stop words and created stop word parsing script

---
+++ src/stopwords.py
@@ -0,0 +1,13 @@
+
+
+def get_stop_words(path : str):
+
+    with open(path, "r") as file:
+        words = [ w.strip("\n") for w in file.readlines() ] 
+
+    return words
+
+
+if __name__ == "__main__":
+    print (get_stop_words("../assets/stop-words.txt"))
+    print (len(get_stop_words("../assets/stop-words.txt")))
