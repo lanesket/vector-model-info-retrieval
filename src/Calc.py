28bc071f5b9e598bcd25035a1e2bfe2abcc338cf
yourwifeisdead <gamesrud@yandex.ru>
2020-03-13 20:24:36 +0100

    rem Calc

--- src/Calc.py
+++
@@ -1,8 +0,0 @@
-class Calc:
-    @staticmethod
-    def word_count_in_doc(word: str, doc_path: str) -> int:
-        """
-            Number of times word occured in doc
-        """
-        # TODO: store preprocessed data in db
-        pass
