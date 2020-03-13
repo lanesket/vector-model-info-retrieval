a97673a820a8a653459b69b44b41b3c4c9cf9c91
yourwifeisdead <gamesrud@yandex.ru>
2020-03-13 20:22:54 +0100

    renamed to VM

---
+++ src/VectorModel.py
@@ -0,0 +1,8 @@
+class VectorModel:
+    @staticmethod
+    def word_count_in_doc(word: str, doc_path: str) -> int:
+        """
+            Number of times word occured in doc
+        """
+        
+        
