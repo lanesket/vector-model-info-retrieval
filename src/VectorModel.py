a443aec4ffb32a36b09925fbbd12e84b79e2df45
yourwifeisdead <gamesrud@yandex.ru>
2020-05-08 19:30:43 +0200

    Generating vectors for all the files

--- src/VectorModel.py
+++ src/VectorModel.py
@@ -1,5 +1,6 @@
 import os
 import math
+import numpy as np
 
 
 class VectorModel:
@@ -7,16 +8,18 @@ class VectorModel:
         self.processed_path = processed_path
         self.doc_count = len(os.listdir(processed_path))
         self.vector_mapping = self._vector_mapping()
+        self.num_terms = len(self.vector_mapping.keys())
         self.tfs, self.dfs = self._generate_tfs_dfs()
 
-    def _vector_mapping(self):
+    def _vector_mapping(self) -> dict:
         """
             Dict with word to its place in the vector
         """
         words = set()
         for file in os.listdir(self.processed_path):
-            with open(f"{self.processed_path}/{file}", 'r') as f:
-                text_words = f.readline().split(' ')
+            doc_path = f"{self.processed_path}/{file}"
+            with open(doc_path, 'r') as f:
+                text_words = f.readline().split()
                 words = words.union(set(text_words))
 
         return dict(zip(words, range(len(words))))
@@ -65,16 +68,37 @@ class VectorModel:
         return math.log(self.doc_count / self.df(term))
 
     def tf_idf_weight(self, term: str, doc_path: str) -> float:
+        """
+            Calculates the tf-idf weight for `term` in document with `doc_path`
+        """
         return self.tf(term, doc_path) * self.idf(term)
+
+    def weights_for_doc(self, doc_path: str):
+        """
+            Creates the vector of tf-idf weights for document with `doc_path`
+        """
+        weights = np.zeros(shape=self.num_terms)
+        with open(doc_path, 'r') as f:
+            text = f.readline()
+            terms = set(text.split())
+            for term in terms:
+                index = self.vector_mapping[term]
+                weights[index] = self.tf_idf_weight(term, doc_path)
+
+        return weights
+
+    def generate_weights(self, weights_path: str):
+        """
+            Saves the vectors for articles to `weights_path`
+        """
+        for file in os.listdir(self.processed_path):
+            doc_path = f"{self.processed_path}/{file}"
+            weights = self.weights_for_doc(doc_path)
+            save_path = f"{weights_path}/{file}"
+            np.save(save_path, weights)
 
 
 if __name__ == "__main__":
     vm = VectorModel('assets/articles/processed')
 
-    print(vm.tf('star',
-                'assets/articles/processed/‘Hidden Figures’ Ties ‘Rogue One’ at Box Office - The New York Times'))
-
-    print(vm.df('star'))
-    print(vm.idf('star'))
-    print(vm.tf_idf_weight(
-        'star', 'assets/articles/processed/‘Hidden Figures’ Ties ‘Rogue One’ at Box Office - The New York Times'))
+    vm.generate_weights('assets/articles/vectors')
