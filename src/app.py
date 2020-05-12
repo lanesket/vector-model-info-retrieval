c2672fd632d4449440007e8a0a941f6e679d0533
Gleb Feorov <fedorgle@fit.cvut.cz>
2020-05-12 08:17:01 +0300

    Added gui support for inverted index search

--- src/app.py
+++ src/app.py
@@ -5,9 +5,10 @@ from string import Template
 import os
 
 vm = VectorModel('assets/articles/processed')
-if not os.listdir('assets/articles/vectors'):
-    vm.generate_weights('assets/articles/vectors')
+# if not os.listdir('assets/articles/vectors'):
+vm.generate_weights('assets/articles/vectors')
 vectors = vm.load_vectors('assets/articles/vectors')
+vm.create_inverted_index(vectors)
 p = Preprocessor("assets/stop-words.txt")
 
 
@@ -18,24 +19,32 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route('/iteration',  methods=['POST', 'GET'])
-def iter():
-    return ""
+
 
 @app.route('/', methods=['POST', 'GET'])
 def main_response():
     query = request.form['query']
+    use_index = ""
+    if request.method == "POST":
+        use_index = request.form.get("use_index")
     processed_query = p.process(query)
     query_vector = vm.query_vectorize(processed_query)
 
     if ":" in query:
         pairs = query.split(" ")
+        terms = []
         for pair in pairs:
             t, d, w = pair.partition(":")
             # vector_mapping returns position of a term in a vector
             # set only weights for the provided terms, since others are 0
             query_vector[vm.vector_mapping[t.lower()]] = int(w)
-    res = vm.find_similar(vectors, query_vector, 7)
+            terms.append(t)
+        query = " ".join(terms)
+
+    if use_index:
+        res = vm.find_similar_with_index(vectors, query, query_vector)
+    else:
+        res = vm.find_similar(vectors, query_vector, 10)
     res = [ r.split("/")[-1] for r in res ]
 
     return render_template("index.html", qres=res)
